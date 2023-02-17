0cb900efe4249cb3dc3c16b6584a96336bf43a2f
Mads Schou Olesen <mschouolesen@gmail.com>
2023-02-17 08:57:18 +0100

    less info in html, more clean so to say

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 "use strict";
 
-window.addEventListener("DOMContentLoaded", setup);
+window.addEventListener("DOMContentLoaded", start);
 
-const HTML = {};
+function start(evt) {
+  addEventListeners();
+}
 
-function setup() {
-  console.log("setup");
-  HTML.input = document.querySelector("#colorwheel");
-  HTML.colorCodes = document.querySelector("#colorCodes");
-  HTML.hexValue = document.querySelector(".hex");
-  HTML.rgbValue = document.querySelector(".rgb");
-  HTML.cssValue = document.querySelector(".css");
-  HTML.hslValue = document.querySelector(".hsl");
-  HTML.colorBox = document.querySelector(".color_box");
-  HTML.colorBody = document.querySelector("body");
+function addEventListeners() {
+  document.querySelector("#my_color_picker").addEventListener("input", getColor);
+}
 
-  HTML.hexValue.textContent = `hex: ${HTML.input.value}`;
-  HTML.rgbValue.textContent = "rgb: 255, 255, 255";
-  HTML.cssValue.textContent = "css: rgb(255, 255, 255)";
-  HTML.hslValue.textContent = "hsl: 0, 0, 100";
-  HTML.colorBox.style.backgroundColor = `${HTML.input.value}`;
+function getColor(evt) {
+  showSelectedColor;
+  evt.target.value;
+}
 
-  getColor();
- 
+function showSelectedColor(hexColor) {
+  console.log(hexColor);
+}
